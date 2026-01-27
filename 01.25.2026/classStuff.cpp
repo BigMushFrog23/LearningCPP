f003934ff6156c1575af6d9193eaf5fe8d33deb6
BigMushFrog23 <popov.jeremy.v@gmail.com>
2026-01-27 15:15:59 +0100

    Studies: Completed the class file by adding File Parsing for our student list in a .txt form.

--- 01.25.2026/classStuff.cpp
+++ 01.25.2026/classStuff.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
 class Student
 {
+
+	// Anything written before "public:" will be private by default.
 
 	string	m_firstName	= "First";
 	string	m_lastName	= "Last";
@@ -82,8 +85,38 @@ public:
 		return m_students;
 	}
 
+	void loadFromFile(const string& fileName)
+	{
+		ifstream fin(fileName); //ifstream means Input File Stream, fin is the name of the the variable (acronym for File In), then we put the argument being the file.
+		string first, last;
+		int id;
+		float average;
+
+		// Error to check if we managed to open the file / find it.
+		if (!fin.is_open())
+		{
+			cerr << "Error: Could not open file " << fileName << endl;
+			return;
+		}
+
+		// If we use ">>", it means we are putting the current value/token of fin into temp
+		// "<<" means we are putting the value of a variable into the cout, which prints it.
+		// It basically means, get the next token seperated by whitespace or a new line.
+		while (fin >> first)
+		{
+			fin >> last >> id >> average; // While we still find first names, we input the tokens one by one into their variables (first, last, id, average).
+										  // Since every student in the file is FirstName -> LastName -> ID -> Average, we always get all the values of one student at a time.
+										  // And we also loop so we do the first values, then go to the next available one after the student is done.
+
+			addStudent(Student(first, last, id, average));
+
+		}
+	}
+
+	// Here we make a print function to print out the entire student information at once.
 	void print() const
 	{
+		// auto will automatically assign the data type of the value inputed into it, since we never modify its value we will do a const, and since its an object, we pass a reference.
 		for (const auto& s : m_students)
 		{
 			s.print();
@@ -95,6 +128,11 @@ public:
 int main(int argc, char *argv[])
 {
 
+	Course c("COMP 4300");
+	c.loadFromFile("StudentList.txt"); // We parse in the information from the txt file
+	c.print();
+
+	/*
 	Student s1;
 	Student s2("Jeremy", "Popov", 1, 3.14f); // If it's a float, I gotta add 'f' at the end to precise its a float, otherwise it's a double.
 	Student s3("BigMushFrog23", "UMR", 2, 3.15f);
@@ -106,6 +144,7 @@ int main(int argc, char *argv[])
 	comp4300.addStudent(Student("New", "Student", 3, 4));
 
 	comp4300.print();
+	*/
 
 	return 0;
 
